7165577b4f0822ebe851545f48b89c434ab50476
Luis Rangel <luis.rangel@telus.com>
2022-10-05 08:10:53 -0600

    feat: add accounts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, AccountsModule],
   controllers: [AppController],
   providers: [AppService],
 })
